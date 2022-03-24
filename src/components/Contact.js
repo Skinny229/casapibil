094e233e62a261fc74c7bc7857cf07be45fbfb95
Rodrigo Lopez <skinny229@gmail.com>
2022-03-24 14:40:24 -0400

    added textbox to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,7 +13,8 @@ class Contact extends Component {
             message: "",
         }
 
-        this.handleClickSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClickSubmit = this.handleClickSubmit.bind(this);
     }
 
 
@@ -23,8 +24,35 @@ class Contact extends Component {
         var value = target.value;
         const name = target.name;
 
+        console.log("phone")
+        if(name == "phone")
+            value = this.formatPhone(value)
+
         this.setState({ [name]: value });
     }
+
+    formatPhone(value) {
+        if (!value) return value;
+
+        const phoneNumber = value.replace(/[^\d]/g, '');
+
+        const phoneNumberLength = phoneNumber.length;
+
+
+
+        if (phoneNumberLength < 4) return phoneNumber;
+
+
+        if (phoneNumberLength < 7) {
+            return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
+        }
+
+        return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(
+            3,
+            6
+        )}-${phoneNumber.slice(6, 10)}`;
+    }
+
 
 
     handleClickSubmit() {
@@ -36,15 +64,17 @@ class Contact extends Component {
         return (
             <section id="contact" className=''>
                 <h2 class="centered">Contact Form</h2>
+                <h5 class="centered">Need a bigger order? Catering? Comments or concerns? Send us a message!</h5>
 
-                <input onChange={this.handleInputChange} value={this.state.contactName} name="contantName" placeholder="Your Name" />
-
-
-                <input onChange={this.handleInputChange} value={this.state.contactEmail} name="email" placeholder="Your email" />
+                <input onChange={this.handleChange} value={this.state.name} name="name" placeholder="Your Name" />
 
 
-                <input inputmode="numeric" pattern="[0-9]*" onChange={this.handleInputChange} value={this.state.contactPhone} name="contactPhone" placeholder="Your Phone Number" />
+                <input onChange={this.handleChange} value={this.state.email} name="email" placeholder="Your email" /> 
 
+
+                <input inputmode="numeric" pattern="[0-9]*" onChange={this.handleChange} value={this.state.phone} name="phone" placeholder="Your Phone Number" />
+             
+                <textarea onChange={this.handleChange} className='textBoxContact' placeholder='Write your message here!' name='message'/>
 
                 <div class="form-send">
                     <button type="submit" class="btn btn-large" onClick={this.handleClickSubmit} >Send Message</button>
